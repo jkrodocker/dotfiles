ef2260bc47d98c3800e1b7dcd388c33544b2f8e4
John Rodocker <173859060+jkrodocker@users.noreply.github.com>
2026-01-14 11:19:50 -0700

    Rename bootstrap.sh

--- remote_install.sh
+++ remote_install.sh
@@ -1,9 +1,9 @@
 # ================================================================================
-# bootstrap.sh
+# remote_install.sh
 # ================================================================================
 # This script sets up from a clean installation of WSL Ubuntu, but should be
 # idempotent. This can be pulled by itself and run to automatically pull and
-# configure the remainder of the setup
+# configure the remainder of the setup.
 # ================================================================================
 
 # Update and upgrade all packages
