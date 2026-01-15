92c320980c4c7a30d7e95f3d67f94cb744ed60c6
John Rodocker <173859060+jkrodocker@users.noreply.github.com>
2026-01-15 02:26:26 -0700

    [BROKEN] Begin symlink automation

--- install.sh
+++ install.sh
@@ -1,1 +1,35 @@
-# TODO: Automate symlinking of dotfiles
+#!/usr/bin/env bash
+# ================================================================================
+# install.sh
+# ================================================================================
+# This script symlinks the dotfiles in this repo to the user's home directory to
+# be sourced on startup.
+# ================================================================================
+
+cd "$(dirname ${BASH_SOURCE[0]})"
+
+create_symlinks() {
+    declare -a FILES_TO_SYMLINK=(
+        ".bash_aliases"
+        ".bashrc"
+        ".inputrc"
+        ".gitconfig"
+    )
+    
+    file=""
+
+    for file in ${FILES_TO_SYMLINK[@]}; do
+        if [ -e $file ]; then
+            read -r -n 1
+            
+        fi
+    done
+}
+
+main() {
+
+
+    create_symlinks
+}
+
+main "$@"
