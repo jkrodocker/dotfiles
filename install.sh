650a1c94f8459901014bf8198abe35fda35a4879
John Rodocker <johnkrodocker@gmail.com>
2026-02-03 13:50:00 -0700

    Add option parsing and dry-run output to install script

--- install.sh
+++ install.sh
@@ -6,7 +6,42 @@
 # be sourced on startup.
 # ================================================================================
 
-cd "$(dirname ${BASH_SOURCE[0]})"
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hd]
+This is the install script for my dotfiles.
+
+    -h  display this help and exit
+    -d  perform a dry-run rather than full installation
+EOF
+}
+
+# Handle options
+dry_run=0
+
+while getopts hd: opt; do
+    case $opt in
+        h)
+            show_help
+            exit 0
+            ;;
+        d)
+            dry_run=1
+            ;;
+        *)
+            show_help >&2
+            exit 1
+            ;;
+    esac
+done
+
+
+
+# Ensure all commands run relative to ~/.dotfiles
+cd "$(dirname "${BASH_SOURCE[0]}")" || {
+    echo "Failed to cd into dotfiles directory" >&2
+    exit 1
+}
 
 create_symlinks() {
     declare -a FILES_TO_SYMLINK=(
@@ -16,29 +51,42 @@ create_symlinks() {
         ".gitconfig"
     )
     
-    file=""
+    local i=""
+    local source_file=""
+    local target_file=""
 
-    for file in ${FILES_TO_SYMLINK[@]}; do
-        if [ -e $file ]; then
-            read -r -n 1
-            
+    for i in "${FILES_TO_SYMLINK[@]}"; do
+
+        source_file="$i"
+        target_file="$HOME/$i"
+
+        if [ -e "$source_file" ]; then
+            # Ask for confirmation if target already exists
+            if [ -e "$target_file" ] || [ -L "$target_file" ]; then
+                # TODO: Divide confirmation ask into separate function
+                printf "%s already exists. Overwrite it? (y/n): " "${target_file}" >&2
+                read -r -n 1 REPLY  
+                printf "\n" >&2
+
+                if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then
+                    continue
+                fi
+            fi
+
+            # TODO: CHECK IF PREFORMING DRY-RUN BEFORE REAL LINK BELOW
+            # ln -sfn "$PWD/$source_file" "$target_file"
+
+            printf "    %s --> %s/%s\n" "$target_file" "$PWD" "$source_file" >&2
+        else
+            printf "Could not find %s\n" "$source_file" >&2
         fi
     done
 }
 
-configure_wsl() {
-    # Replace /etc/wsl.conf for current WSL instance
-
-}
-
 main() {
     # TODO: Establish way to automatically distinguish between Linux Mint setup and WSL setup
-    case "$OSTYPE" in
-        linux*)
-            ;;
-    esac
 
-    #create_symlinks
+    create_symlinks "$@"
 }
 
 main "$@"
