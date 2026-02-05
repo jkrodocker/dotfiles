9c856a91bb57640f5fe416f303bb2040f617ef8b
John Rodocker <johnkrodocker@gmail.com>
2026-02-04 23:35:59 -0700

    Add setup files used previously

---
+++ programs/curl-install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Upgrade curl to v8.18.0
+sudo apt-get install -y nghttp2 libnghttp2-dev libssl-dev libpsl-dev build-essential wget
+wget https://curl.se/download/curl-8.14.1.tar.xz
